c2ca816468cbea1775c24658295dbf121c46eab3
Ahmad Aldayem <ahmadaldayem@ahmads-mbp.attlocal.net>
2017-03-31 22:05:21 -0700

    final

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '<n9aIHeQj5e2J9jpBDRkDumiie>',
+  consumer_secret: '<Cp6QXPV7n7CTaZsiSuVFLCzsDIeKXW64cMWkf2ZvCCnukpLFGt>',
+  access_token_key: '<1281677071-5N6dSW4oLFIFx9VPxGrevUtCl2Rlu2TsXTCyWLx>',
+  access_token_secret: '<h2kgZOjwpLKsZqCEWHxj8E7eYXLIQcEYkzwLa8wbHHWoW>',
+}
